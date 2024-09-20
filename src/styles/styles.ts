75e85f21f4e71e1c3153de3ecc50e194b1031d54
Artur <71281020+Artur0412-KU@users.noreply.github.com>
2024-09-20 19:11:01 +0300

    modified:   App.tsx 	modified:   app.json 	modified:   package-lock.json 	modified:   package.json 	new file:   src/styles/styles.ts

---
+++ src/styles/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { Roboto_400Regular } from "@expo-google-fonts/roboto";
+
+export const styles = StyleSheet.create({
+    linearGradient: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'Roboto_400Regular',
+    },
+  });
+  
